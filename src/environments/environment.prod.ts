247c6fdb85987f12ea402daa33f5f14b448de65b
kamal <kamal.mewada.nitt@gmail.com>
2022-07-13 13:14:00 +0530

    chnage ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,6 @@ export const environment = {
   ws: 'ws://trucku.in:5001/',
   trucku_url: 'http://trucku.in:8081/',
   gps_url: 'http://13.229.178.235:4242/',
- //prod
-  url: 'http://15.206.26.250:3011/',
-  img_url:"http://15.206.26.250:7652/",
-
-  //test
-  //url: 'http://205.147.99.129:3006/',
-  //img_url:"http://lmstest.umbrellaprotectionsystems.com:7653/"
+  url: 'http://205.147.99.129:3006/',
+  img_url:"http://lmstest.umbrellaprotectionsystems.com:7653/"
 };
