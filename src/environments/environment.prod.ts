c94ed52b47d9ccb8aa7fabe6c10820a347703b02
kamal <kamal.mewada.nitt@gmail.com>
2022-07-20 17:33:41 +0530

    auth 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment = {
   trucku_url: 'http://trucku.in:8081/',
   gps_url: 'http://13.229.178.235:4242/',
  //prod
-  //url: 'http://15.206.26.250:3011/',
-  //img_url:"http://15.206.26.250:7652/",
+  url: 'http://15.206.26.250:3011/',
+  img_url:"http://15.206.26.250:7652/",
 
   //test
-  url: 'http://205.147.99.129:3006/',
-  img_url:"http://lmstest.umbrellaprotectionsystems.com:7653/"
+  //url: 'http://205.147.99.129:3006/',
+  //img_url:"http://lmstest.umbrellaprotectionsystems.com:7653/"
 };
